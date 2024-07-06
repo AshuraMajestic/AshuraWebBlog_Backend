7b1baabc5f7ee7ab89026ee332fcd6205a53d648
Ashura <ashuramajestic@gmail.com>
2024-07-06 15:55:12 +0530

    ashura

--- setup_and_run.ps1
+++
@@ -1,7 +0,0 @@
-
-mkdir -p .postgresql
-
-Invoke-WebRequest -Uri https://cockroachlabs.cloud/clusters/d08078a8-1e28-4cb6-886f-5ccd2a4b5616/cert -OutFile .postgresql/root.crt
-
-
-gunicorn ashurawebblog.wsgi:application
